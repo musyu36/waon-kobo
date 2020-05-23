510d825493335238e9b43943f694e7b49c754cbf
mimura <musyu36@gmail.com>
2020-05-23 18:47:57 +0900

    メジャーコードに対応

--- src/components/ButtonSet.js
+++ src/components/ButtonSet.js
@@ -1,6 +1,9 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useState } from "react";
 import "../styles/ButtonSet.css";
 import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+
 import { UPDATE_CHORD } from "../actions";
 import reducer from "../reducers";
 
@@ -16,6 +19,39 @@ import GS from "../notes/G-s.mp3";
 import A from "../notes/A.mp3";
 import AS from "../notes/A-s.mp3";
 import B from "../notes/B.mp3";
+
+function getModalStyle() {
+  const top = 20;
+  const left = 30;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: "auto",
+    backgroundColor: "#f2ebbf",
+    borderTop: "2px solid #f06060",
+    boxShadow: theme.shadows[1],
+    padding: theme.spacing(2, 4, 3),
+    outline: "none",
+  },
+  btnCancel: {
+    marginRight: 8,
+    color: "#8cbeb2",
+    border: "1px solid #8cbeb2",
+  },
+  btnOk: {
+    backgroundColor: "#8cbeb2",
+    color: "#f2ebbf",
+    border: "1px solid #8cbeb2",
+  },
+}));
 
 const ButtonsSet = () => {
   const initialState = [0, 4, 7];
@@ -35,22 +71,150 @@ const ButtonsSet = () => {
   notes.push(new Audio(AS)); // 10
   notes.push(new Audio(B)); // 11
 
+  // const notesString = [
+  //   "C",
+  //   "C♯",
+  //   "D",
+  //   "D♯",
+  //   "E",
+  //   "F",
+  //   "F♯",
+  //   "G",
+  //   "G♯",
+  //   "A",
+  //   "A♯",
+  //   "B",
+  // ];
+
+  const initialNotesState = [
+    { name: "C", value: 0, checked: true },
+    { name: "C♯", value: 1, checked: false },
+    { name: "D", value: 2, checked: false },
+    { name: "D♯", value: 3, checked: false },
+    { name: "E", value: 4, checked: false },
+    { name: "F", value: 5, checked: false },
+    { name: "F♯", value: 6, checked: false },
+    { name: "G", value: 7, checked: false },
+    { name: "G♯", value: 8, checked: false },
+    { name: "A", value: 9, checked: false },
+    { name: "A♯", value: 10, checked: false },
+    { name: "B", value: 11, checked: false },
+  ];
+
+  // ルート音
+  const [notesStrings, setNotesStrings] = useState(initialNotesState);
+  const [currentNote, setCurrentNote] = useState("C");
+  // 構成
+  const [chordStrings, setChordStrings] = useState();
+  const [currentChord, setCurrentChord] = useState("maj");
+
+  const [open, setOpen] = useState(false);
+
+  const classes = useStyles();
+  const [modalStyle] = React.useState(getModalStyle);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleRootRadioClick = (e) => {
+    e.preventDefault();
+    var name = e.target.name;
+    var value = notesStrings.map((item) => {
+      return {
+        name: item.name,
+        value: item.value,
+        checked: item.name === name ? true : false,
+      };
+    });
+    setNotesStrings(value);
+  };
+
   const playChord = () => {
     state.map((value) => {
       notes[value].play();
     });
   };
 
-  const selectRoot = () => {
-    dispatch({ type: UPDATE_CHORD, chord: [2, 6, 9] });
+  const selectRoot = (e) => {
+    e.preventDefault();
+    var rootNum = 0;
+
+    // 選択中のルート音を更新
+    notesStrings.map((note, index) => {
+      if (note.checked) {
+        rootNum = note.value;
+        return setCurrentNote(note.name);
+      }
+    });
+
+    var nextChord = [];
+    switch (currentChord) {
+      case "maj":
+        nextChord.push(rootNum);
+        nextChord.push(rootNum + 4);
+        nextChord.push(rootNum + 7);
+        break;
+      default:
+        break;
+    }
+    dispatch({ type: UPDATE_CHORD, chord: nextChord });
+    handleClose();
   };
+
   const selectChord = () => {};
+
+  const body = (
+    <div style={modalStyle} className={classes.paper}>
+      <h4>基音</h4>
+      <div className="modal-notes">
+        <form action="">
+          {notesStrings.map((note, index) => (
+            <label key={index} style={{ marginRight: "8px" }}>
+              <input
+                type="radio"
+                name={note.name}
+                value={note.value}
+                checked={note.checked}
+                onChange={handleRootRadioClick}
+              />
+              {note.name}
+            </label>
+          ))}
+        </form>
+      </div>
+      <div className="btn-set">
+        <Button
+          onClick={handleClose}
+          className={classes.btnCancel}
+          variant="outlined"
+        >
+          キャンセル
+        </Button>
+        <Button
+          onClick={selectRoot}
+          className={classes.btnOk}
+          variant="contained"
+        >
+          決定
+        </Button>
+      </div>
+    </div>
+  );
+
   return (
     <div className="button-set">
       <Button className="chord-button" variant="outlined" onClick={playChord}>
-        C
+        {currentNote}
       </Button>
-      <Button onClick={selectRoot}>基音</Button>
+      <Button onClick={handleOpen}>基音</Button>
+      <Modal open={open} onClose={handleClose}>
+        {body}
+      </Modal>
       <Button onClick={selectChord}>構成</Button>
     </div>
   );
