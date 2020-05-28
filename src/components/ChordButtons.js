b9a66097efec080eb2c15062e07fe6fc405e6c67
mimura <musyu36@gmail.com>
2020-05-28 18:47:22 +0900

    鍵盤を追加

--- src/components/ChordButtons.js
+++ src/components/ChordButtons.js
@@ -1,15 +1,21 @@
 import React from "react";
 import "../styles/ChordButtons.css";
 import ButtonSet from "./ButtonSet.js";
+import KeyBoard from "./KeyBoard.js";
 
 const ChordButtons = () => {
   return (
-    <div className="chord-buttons">
-      <ButtonSet />
-      <ButtonSet />
-      <ButtonSet />
-      <ButtonSet />
-    </div>
+    <>
+      <div className="chord-buttons">
+        <ButtonSet />
+        <ButtonSet />
+        <ButtonSet />
+        <ButtonSet />
+      </div>
+      <div className="key-board">
+        <KeyBoard />
+      </div>
+    </>
   );
 };
 
---
+++ src/components/KeyBoard.js
@@ -0,0 +1,70 @@
+import React from "react";
+import "../styles/KeyBoard.css";
+
+const KeyBoard = () => {
+  return (
+    <div className="keyboard">
+      <div className="key">
+        <div className="white_key 0"></div>
+        <div className="black_key 1"></div>
+      </div>
+      <div className="key">
+        <div className="white_key 2"></div>
+        <div className="black_key 3"></div>
+      </div>
+      <div className="key">
+        <div className="white_key 4"></div>
+      </div>
+      <div className="key">
+        <div className="white_key 5"></div>
+        <div className="black_key 6"></div>
+      </div>
+      <div className="key">
+        <div className="white_key 7"></div>
+        <div className="black_key 8"></div>
+      </div>
+      <div className="key">
+        <div className="white_key 9"></div>
+        <div className="black_key 10"></div>
+      </div>
+      <div className="key">
+        <div className="white_key 11"></div>
+      </div>
+      <div className="key">
+        <div className="white_key 12"></div>
+        <div className="black_key 13"></div>
+      </div>
+      <div className="key">
+        <div className="white_key 14"></div>
+        <div className="black_key 15"></div>
+      </div>
+      <div className="key">
+        <div className="white_key 16"></div>
+      </div>
+      <div className="key">
+        <div className="white_key 17"></div>
+        <div className="black_key 18"></div>
+      </div>
+      <div className="key">
+        <div className="white_key 19"></div>
+        <div className="black_key 20"></div>
+      </div>
+      <div className="key">
+        <div className="white_key 21"></div>
+        <div className="black_key 22"></div>
+      </div>
+      <div className="key">
+        <div className="white_key 23"></div>
+      </div>
+      <div className="key">
+        <div className="white_key 24"></div>
+        <div className="black_key 25"></div>
+      </div>
+      <div className="key">
+        <div className="white_key 26"></div>
+      </div>
+    </div>
+  );
+};
+
+export default KeyBoard;
